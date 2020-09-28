8efc99eca64b02e759b5e001d1f1ecbf20ad0dff
Viktor Török <torokv@vnet.hu>
2020-09-28 10:15:05 +0200

    Adding example2 and example3
    Check if the list is empty or not.

--- src/ArrayListTest.java
+++ src/ArrayListTest.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
-public class ArrayListTest {
-
+public class ArrayListTest
+{
 	private static void example1()
 	{
 		ArrayList<String> list = new ArrayList<>();
@@ -19,12 +19,52 @@ public class ArrayListTest {
 		}
 	}
 	
+	private static void example2()
+	{
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		if (list.size() == 0)
+		{
+			System.out.println("The list is empty.");
+		}
+		
+		if (list.isEmpty())
+		{
+			System.out.println("The list is empty.");
+		}
+	}
+	
+	private static void example3()
+	{
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		list.add(100);
+		
+		if (list.size() != 0)
+		{
+			System.out.println("The list is not empty.");
+		}
+		
+		if (!list.isEmpty())
+		{
+			System.out.println("The list is not empty.");
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("ArrayList test program");
 
 		System.out.println("Example 1");
 		example1();
+		System.out.println();
+		
+		System.out.println("Example 2");
+		example2();
+		System.out.println();
+		
+		System.out.println("Example 3");
+		example3();
 		System.out.println();
 	}
 }
